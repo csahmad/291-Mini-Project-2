827f1e7ccbe3f9c3fd3493b88a899c6e2e75d959
jessicaprt <jessicaprieto31@gmail.com>
2017-03-22 14:15:26 -0600

    write text, name, location to terms.txt

--- Phase1Source/Main.py
+++ Phase1Source/Main.py
@@ -11,9 +11,17 @@ class Main:
         return text.split()
     
     @staticmethod
-    def writeTermToFile(file, terms):
+    def writeTermToFile(file, terms, tid):
         for i in terms:
-            file.write('t-' + i + '\r\n')
+            file.write('t-{0}:{1}\r\n'.format(i.lower(),tid))
+
+    @staticmethod
+    def writeUNameToFile(file, uname, tid):
+        file.write('n-{0}:{1}\r\n'.format(uname,tid))
+
+    @staticmethod
+    def writeULocationToFile(file, ulocation, tid):
+        file.write('l-{0}:{1}\r\n'.format(ulocation,tid))
 
     @staticmethod
     def xmlParser(file, line):
@@ -21,10 +29,18 @@ class Main:
         # print(root.tag)
         for child in root:
             # f = open ('terms.txt')
+            if child.tag == "id":
+                tid = child.text
             if child.tag == "text":
                 parsedText = Main.readText(child.text)
-                Main.writeTermToFile(file, parsedText)
-                print(parsedText)
+                Main.writeTermToFile(file, parsedText, tid)
+            if child.tag == "user":
+                for grandchild in child:
+                    if grandchild.tag == "name":
+                        Main.writeUNameToFile(file, grandchild.text, tid)
+                    if grandchild.tag == "location":
+                        Main.writeULocationToFile(file, grandchild.text, tid)
+
             print(child.tag, child.text)
 
     @staticmethod
@@ -36,6 +52,7 @@ class Main:
             if line.startswith("<status>"):
                 print(line)
                 Main.xmlParser(f, line)
+        f.close()
 
 if __name__ == "__main__":
 	Main.main()
