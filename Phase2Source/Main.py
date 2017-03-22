56c9ef71163239bf67a7169edd19ef6f13a62ac6
ahmad <ahmad@ualberta.ca>
2017-03-22 13:01:30 -0600

    Added method placeholders for creating indices

--- Phase2Source/Main.py
+++ Phase2Source/Main.py
@@ -5,6 +5,26 @@ class Main:
 	def main():
 		"""Run the program"""
 
+		Main.makeTweetsIndex
+		Main.makeTermsIndex
+		Main.makeDatesIndex
+
+	@staticmethod
+	def makeTweetsIndex():
+		"""Make the index for tweets.txt"""
+
+		pass
+
+	@staticmethod
+	def makeTermsIndex():
+		"""Make the index for terms.txt"""
+
+		pass
+
+	@staticmethod
+	def makeDatesIndex():
+		"""Make the index for dates.txt"""
+
 		pass
 
 if __name__ == "__main__":
