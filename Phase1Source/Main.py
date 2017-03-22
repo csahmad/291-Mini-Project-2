9bb875fe63d2f97c8a0a2bd2e5dee0a00f2a9a76
jessicaprt <jessicaprieto31@gmail.com>
2017-03-22 13:04:16 -0600

    modify to read from stdin

--- Phase1Source/Main.py
+++ Phase1Source/Main.py
@@ -1,25 +1,25 @@
 import xml.etree.ElementTree as ET
+import sys
+import fileinput
 
 class Main:
     """Run the program"""
     @staticmethod
-    def xmlParser():
-        data = {}
-        with open('10.txt') as f:
-            for line in f:
-                print(line)
-                if line.startswith("<status>"):
-                    root = ET.fromstring(line)
-                    # print(root.tag)
-                    for child in root:
-                        print(child.tag, child.text)
+    def xmlParser(line):
+        if line.startswith("<status>"):
+            root = ET.fromstring(line)
+            # print(root.tag)
+            for child in root:
+                print(child.tag, child.text)
                         
-                print("\n")
+        print("\n")
 
     @staticmethod
     def main():
         """Run the program"""
-        Main.xmlParser()
+        for line in fileinput.input():
+            print(line)
+            Main.xmlParser(line)
 
 if __name__ == "__main__":
 	Main.main()
