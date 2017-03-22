b237e0d8f349a00980327df4b4a0eda88315ed8e
jessicaprt <jessicaprieto31@gmail.com>
2017-03-22 12:51:24 -0600

    parsed xml per line

--- Phase1Source/Main.py
+++ Phase1Source/Main.py
@@ -1,11 +1,25 @@
+import xml.etree.ElementTree as ET
+
 class Main:
-	"""Run the program"""
+    """Run the program"""
+    @staticmethod
+    def xmlParser():
+        data = {}
+        with open('10.txt') as f:
+            for line in f:
+                print(line)
+                if line.startswith("<status>"):
+                    root = ET.fromstring(line)
+                    # print(root.tag)
+                    for child in root:
+                        print(child.tag, child.text)
+                        
+                print("\n")
 
-	@staticmethod
-	def main():
-		"""Run the program"""
-
-		pass
+    @staticmethod
+    def main():
+        """Run the program"""
+        Main.xmlParser()
 
 if __name__ == "__main__":
 	Main.main()
