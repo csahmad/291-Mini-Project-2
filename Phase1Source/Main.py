66391927089caed4b157597e13720dccc9f74b4f
jessicaprt <jessicaprieto31@gmail.com>
2017-03-22 14:33:31 -0600

    write dates and tweets to dates.txt and tweets.txt

--- Phase1Source/Main.py
+++ Phase1Source/Main.py
@@ -24,7 +24,15 @@ class Main:
         file.write('l-{0}:{1}\r\n'.format(ulocation,tid))
 
     @staticmethod
-    def xmlParser(file, line):
+    def writeDateToFile(file, date, tid):
+        file.write('{0}:{1}\r\n'.format(date,tid))
+
+    @staticmethod
+    def writeTweetToFile(file, tweet, tid):
+        file.write('{0}:{1}\r\n'.format(tid,tweet))
+
+    @staticmethod
+    def xmlParser(fterms, fdates, ftweets, line):
         root = ET.fromstring(line)
         # print(root.tag)
         for child in root:
@@ -32,27 +40,36 @@ class Main:
             if child.tag == "id":
                 tid = child.text
             if child.tag == "text":
+                Main.writeTweetToFile(ftweets, child.text, tid)
                 parsedText = Main.readText(child.text)
-                Main.writeTermToFile(file, parsedText, tid)
+                Main.writeTermToFile(fterms, parsedText, tid)
             if child.tag == "user":
                 for grandchild in child:
                     if grandchild.tag == "name":
-                        Main.writeUNameToFile(file, grandchild.text, tid)
+                        Main.writeUNameToFile(fterms, grandchild.text, tid)
                     if grandchild.tag == "location":
-                        Main.writeULocationToFile(file, grandchild.text, tid)
+                        Main.writeULocationToFile(fterms, grandchild.text, tid)
+            
+            if child.tag == "created_at":
+                Main.writeDateToFile(fdates, child.text, tid)
 
             print(child.tag, child.text)
 
     @staticmethod
     def main():
         """Run the program"""
-        f = open('../Output/terms.txt', 'w')
+        f1 = open('../Output/terms.txt', 'w')
+        f2 = open('../Output/dates.txt', 'w')
+        f3 = open('../Output/tweets.txt', 'w')
+        
 
         for line in fileinput.input():
             if line.startswith("<status>"):
                 print(line)
-                Main.xmlParser(f, line)
-        f.close()
+                Main.xmlParser(f1, f2, f3, line)
+        f1.close()
+        f2.close()
+        f3.close()
 
 if __name__ == "__main__":
 	Main.main()
