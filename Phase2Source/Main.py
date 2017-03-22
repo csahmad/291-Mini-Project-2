2d796d934c39775c0edab3f367633dd39e089e95
ahmad <ahmad@ualberta.ca>
2017-03-22 13:21:07 -0600

    Added methods to convert data into form db_load can work with

--- Phase2Source/Main.py
+++ Phase2Source/Main.py
@@ -27,5 +27,28 @@ class Main:
 
 		pass
 
+	@staticmethod
+	def getKeyData(string):
+		"""
+		Given a string with a key and the corresponding data in the form
+		"key:data", convert the data into a form db_load works with and return
+		a tuple in the form (key, data)
+		"""
+
+		string = Main.convertSpecialCharacters(string)
+		colonIndex = string.index(":")
+		key = string[:colonIndex]
+		data = string[colonIndex + 1:]
+		return (key, data)
+
+	@staticmethod
+	def convertSpecialCharacters(string):
+		"""
+		Convert special characters in the given string to a form db_load works
+		with
+		"""
+
+		return string.replace("\\", "&92;")
+
 if __name__ == "__main__":
 	Main.main()
