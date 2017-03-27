925e5936be32dce7923adca468dcb14280f71c95
ahmad <ahmad@ualberta.ca>
2017-03-27 14:17:31 -0600

    Updated phase 2 output

--- Phase3Source/Main.py
+++ Phase3Source/Main.py
@@ -14,11 +14,18 @@ class Main:
 		tweetsDatabase.open(tweetsIndex, db.DB_HASH, db.DB_CREATE)
 		tweetsCursor = tweetsDatabase.cursor()
 
+		datesIndex = OUTPUT_FOLDER + "da.idx"
+		datesDatabase = db.DB()
+		datesDatabase.open(datesIndex, db.DB_HASH, db.DB_CREATE)
+		datesCursor = datesDatabase.cursor()
+
 		firstIndex = tweetsCursor.first()
-		firstTweet = firstIndex[0].decode("utf-8")
-		firstTweetLocation = firstIndex[1].decode("utf-8")
-		print(firstTweet)
-		print(firstTweetLocation)
+		firstTweet = firstIndex[1]
+		firstTweetLocation = firstIndex[0]
+		print(firstTweet.decode("utf-8"))
+		print(firstTweetLocation.decode("utf-8"))
+		date = datesDatabase.get(firstTweetLocation)
+		print(date[0].decode("utf-8"))
 
 		tweetsCursor.close()
 		tweetsDatabase.close()
